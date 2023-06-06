2d400ee75a8400a640b4c81f06abd59a6debe774
Satyam <tsingh30399@gmail.com>
2023-06-07 01:26:43 +0530

    Updated code

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const currentProductImg = document.querySelector(".productImg")
 const currentProductTitle = document.querySelector(".productTitle")
 const currentProductPrice = document.querySelector(".productPrice")
 const currentProductColors = document.querySelectorAll(".color")
-const currentProductSizes = document.querySelectorAll(".sizes")
+const currentProductSizes = document.querySelectorAll(".size")
 
 menuItems.forEach((item, index) =>{
     item.addEventListener("click", () => {
@@ -107,4 +107,26 @@ currentProductColors.forEach((color, index)=>{
     color.addEventListener("click", () =>{
         currentProductImg.src = choosenProduct.colors[index].img
     })
+})
+
+currentProductSizes.forEach((size, index)=>{
+    size.addEventListener("click", () => {
+        currentProductSizes.forEach((size) => {
+            size.style.backgroundColor = "white"
+            size.style.color = "black"
+        })
+        size.style.backgroundColor = "black"
+        size.style.color = "white"
+    } )
+})
+
+const productButton = document.querySelector(".productButton")
+const payment = document.querySelector(".payment")
+const close = document.querySelector(".close")
+
+productButton.addEventListener("click", ()=>{
+    payment.style.display = "flex"
+})
+close.addEventListener("click", ()=>{
+    payment.style.display = "none"
 })
